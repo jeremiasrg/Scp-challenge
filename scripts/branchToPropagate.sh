4c8c8a740dfbb6f1e5d47ac5e61bfa86f579c6e5
Jeremy <jeremiasrg@gmail.com>
2022-05-15 22:28:36 +0200

    first commit

--- scripts/branchToPropagate.sh
+++ scripts/branchToPropagate.sh
@@ -3,14 +3,14 @@
 # filter only "release/" branches
 # remove "release/" from the brach names
 # Sort as number and remove multilines
-eval "releaseBranches=$(git branch -r | grep 'release/' | tr -d 'release/' | sort -n | tr '\n' ',')"
+eval "releaseBranches=$(git branch -r | grep 'release/' | tr -d 'origin/release/' | sort -n | tr '\n' ',')"
 
-# echo $releaseBranches
+echo $releaseBranches
 
 # get first param and remove "origin/release/"
 currentVersion=$(echo $1 | tr -d 'origin/release/')
 
-# echo $currentVersion
+echo $currentVersion
 
 # Remove frist part of branch string 
 branchesToMerge=$( echo $releaseBranches | grep -oP "$currentVersion.*")
